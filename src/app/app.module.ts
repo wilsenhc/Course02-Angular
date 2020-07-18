60b1c164092f678e5e0daa67b2f051cb83bd356c
Wilsen Hernández <wilsenh95@gmail.com>
2020-07-18 18:49:35 -0400

    Single Page Applications Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
@@ -45,7 +46,10 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FlexLayoutModule,
     AppRoutingModule,
   ],
-  providers: [DishService],
+  providers: [
+    DishService,
+    PromotionService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
